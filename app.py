8d34e5bc3ecb8db1378b9eac62379057f0eb50c6
soyeunlee5930 <soyeun.lee5930@gmail.com>
2023-06-08 09:04:45 +0900

    로그인, 회워가입 페이지 update

--- app.py
+++ app.py
@@ -1,72 +1,90 @@
 from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
 import certifi
+
 app = Flask(__name__, static_folder="templates/static")
 
-client = MongoClient('mongodb+srv://sparta:test@cluster0.p5xkuy6.mongodb.net/?retryWrites=true&w=majority', tlsCAFile=certifi.where())
+client = MongoClient(
+    "mongodb+srv://sparta:test@cluster0.p5xkuy6.mongodb.net/?retryWrites=true&w=majority",
+    tlsCAFile=certifi.where(),
+)
 db = client.dbsparta
 
-@app.route('/')
+
+@app.route("/")
 def home():
-   return render_template('pages/login.html')
+    return render_template("pages/login.html")
+
 
 # 글 상세 페이지
-@app.route('/detail')
+@app.route("/detail")
 def detail_page():
-   return render_template('pages/detail.html')
+    return render_template("pages/detail.html")
+
 
 # 회원 가입 페이지
-@app.route('/join')
+@app.route("/join")
 def join_page():
-   return render_template('pages/join.html')
+    return render_template("pages/join.html")
+
 
 # 로그인 페이지
-@app.route('/login')
+@app.route("/login")
 def login_page():
-   return render_template('pages/login.html')
+    return render_template("pages/login.html")
+
 
 # 메인 페이지 (글 목록 페이지)
-@app.route('/main')
+@app.route("/main")
 def main_page():
-   return render_template('pages/main.html')
+    return render_template("pages/main.html")
+
 
 # 마이페이지
-@app.route('/my')
+@app.route("/my")
 def my_page():
-   return render_template('pages/my.html')
+    return render_template("pages/my.html")
+
 
 # 글 작성 페이지
-@app.route('/write')
+@app.route("/write")
 def write_page():
-   return render_template('pages/write.html')
+    return render_template("pages/write.html")
+
 
 # 회원 가입
 @app.route("/user/join", methods=["POST"])
 def join():
-    name_receive = request.form['name_give']
-    
-    doc = {
-        'name' : name_receive,
-    }
+    id_receive = request.form["id_give"]
+    name_receive = request.form["name_give"]
+    password_receive = request.form["password_give"]
 
-    db.fan.insert_one(doc)
+    doc = {"id": id_receive, "name": name_receive, "password": password_receive}
 
-    return jsonify({'msg': '저장 완료'})
+    # db.user.insert_one(doc)
+
+    return jsonify({"msg": "가입 성공!"})
+
 
 # 유저 정보 조회
 @app.route("/user", methods=["GET"])
 def getUser():
-    ns = request.args.get("id", type=str) # 파라미터 받는 부분
+    ns = request.args.get("id", type=str)  # 파라미터 받는 부분
 
-    all_comments = list(db.fan.find({}, {'_id':False}))
-    
-    return jsonify({'result': all_comments})
+    all_comments = list(db.fan.find({}, {"_id": False}))
+
+    return jsonify({"result": all_comments})
 
 
 # 로그인
 @app.route("/user/login", methods=["POST"])
 def login():
-    pass
+    id_receive = request.form["id_give"]
+    password_receive = request.form["password_give"]
+
+    # user = db.users.find_one({"id": id_receive, "password":password_receive})
+
+    return render_template("pages/main.html")
 
 
 # 로그인
@@ -86,35 +104,42 @@ def getPost():
 def deleteUser():
     pass
 
+
 # 지역별 글 목록 조회
 @app.route("/posts/region", methods=["GET"])
 def getPostsWithRegion():
     pass
+
 
 # 글 상세 조회
 @app.route("/posts/detail", methods=["GET"])
 def getPostDetail():
     pass
 
+
 # 댓글 목록 조회
 @app.route("/posts/comment", methods=["GET"])
 def getPostComments():
     pass
+
 
 # 댓글 작성
 @app.route("/posts/comment", methods=["POST"])
 def writeComment():
     pass
 
+
 # 지역별 날씨 조회
 @app.route("/posts/weather", methods=["GET"])
 def getRegionWeather():
     pass
+
 
 # 글 작성
 @app.route("/posts", methods=["POST"])
 def writePost():
     pass
 
-if __name__ == '__main__':
-   app.run('0.0.0.0', port=5000, debug=True)
+
+if __name__ == "__main__":
+    app.run("0.0.0.0", port=5000, debug=True)
